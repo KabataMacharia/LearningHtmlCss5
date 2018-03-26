22394c8a3a04631573ce927e225de71ca45865d3
kabatamacharia <peter.k@mambo.co.ke>
2018-03-26 16:27:45 +0300

    Index file addition

--- index.php
+++ index.php
@@ -1,67 +1,68 @@
 <html>
-<header>
-<link rel = "stylesheet" href ="style.css?ver=1">
-<div id="header">
-<h2>Textured-Stars-Light</h2>
-<h5> Simple. Contemporary. Website Template.</h5>
-</div>
-</header>
-<div id = "menubar">
-<ul id = "menu">
-<li><a href ="home.php">home </li>
-<li><a href ="examples.php"> examples </li>
-<li><a href ="page.php"> a page</li>
-<li><a href ="anotherpage.php"> another page</li>
-<li><a href ="contact.php"> contact us </li>
-</ul> 
-</div>
-</header>
-</br>
-</br>
+<head>
+    <link rel = "stylesheet" href ="style.css?ver=1">
+    <title> Site One</title>
+</head>
 
 <body>
-</br>
-</br>
+<div id="header">
+    <h2>Textured-Stars-Light</h2>
+    <h5> Simple. Contemporary. Website Template.</h5>
+</div>
+<header>
+    <div id = "menubar">
+        <ul id = "menu">
+            <li><a href ="home.php">home </li>
+            <li><a href ="examples.php"> examples </li>
+            <li><a href ="page.php"> a page</li>
+            <li><a href ="anotherpage.php"> another page</li>
+            <li><a href ="contact.php"> contact us </li>
+        </ul>
+    </div>
+</header>
+
 <div id= "content">
-<h3>Welcome to the textured_stars_light template</h3>
-</br>
-<p>
-This standards compliant, simple, fixed width website template is released as an 'open source' design (under a Creative Commons Attribution 3.0 Licence), which means that you are free to download and use it for anything you want (including modifying and amending it). All I ask is that you leave the 'design from HTML5webtemplates.co.uk' link in the footer of the template, but other than that...
+    <h3>Welcome to the textured_stars_light template</h3>
+    <br>
+    <p>
+        This standards compliant, simple, fixed width website template is released as an 'open source' design (under a Creative Commons Attribution 3.0 Licence), which means that you are free to download and use it for anything you want (including modifying and amending it). All I ask is that you leave the 'design from HTML5webtemplates.co.uk' link in the footer of the template, but other than that...
+    </p>
+        <p>
 
-</br>
-</br>
 
-This template is written entirely in HTML5 and CSS, and can be validated using the links in the footer.
-</br>
-</br>
-You can view more free HTML5 web templates here.
-</br>
-</br>
-This template is a fully functional 5 page website, with an examples page that gives examples of all the styles available with this design.</p>
-<h3>Browser Compatibility</h3>
-<p>This template has been tested in the following browsers:</p>
+        This template is written entirely in HTML5 and CSS, and can be validated using the links in the footer.
+        </p>
+    <p>
+        You can view more free HTML5 web templates here.
+    </p>
+    <p>
+        This template is a fully functional 5 page website, with an examples page that gives examples of all the styles available with this design.</p>
+    <h3>Browser Compatibility
+    </h3></h3>
+    <p>This template has been tested in the following browsers:</p>
 
-< div id="list" >
-<ul>
-<li>Internet Explorer 9</li>
-<li>FireFox 25 </li>
-<li>Google Chrome 31 </li>
+    < div id="list" >
+    <ul>
+        <li>Internet Explorer 9</li>
+        <li>FireFox 25 </li>
+        <li>Google Chrome 31 </li>
+    </ul>
 </div>
-</ul>
-</div>
+
+
 <div id="sidebar">
-<h3> Latest News <h3>
-<h4> New Website Launched <h4> </br>
+    <h3> Latest News </h3>
+    <h4> New Website Launched </h4> <br/>
 
-<p> January 1st, 2014 </br>
-2014 sees the redesign of our website. Take a look around and let us know what you think.
-Read more 
-<h3> Latest News <h3>
-<h4> New Website Launched <h4> </br>
+    <p> January 1st, 2014 <br/>
+        2014 sees the redesign of our website. Take a look around and let us know what you think.
+        Read more
+    <h3> Latest News </h3>
+    <h4> New Website Launched </h4> <br/>
 
-<p> January 1st, 2014 </br>
-2014 sees the redesign of our website. Take a look around and let us know what you think.
-Read more </p>
+    <p> January 1st, 2014 <br/>
+        2014 sees the redesign of our website. Take a look around and let us know what you think.
+        Read more </p>
 
 </div>
 </body>
