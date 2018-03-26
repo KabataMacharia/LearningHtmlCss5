faa13a1014945b0cb3ff68ef53a7edb31fb74a79
kabatamacharia <peter.k@mambo.co.ke>
2018-03-26 17:26:52 +0300

    Index page corrections

--- index.php
+++ index.php
@@ -3,67 +3,69 @@
     <link rel = "stylesheet" href ="style.css?ver=1">
     <title> Site One</title>
 </head>
-
 <body>
-<div id="header">
-    <h2>Textured-Stars-Light</h2>
-    <h5> Simple. Contemporary. Website Template.</h5>
-</div>
-<header>
-    <div id = "menubar">
-        <ul id = "menu">
-            <li><a href ="home.php">home </li>
-            <li><a href ="examples.php"> examples </li>
-            <li><a href ="page.php"> a page</li>
-            <li><a href ="anotherpage.php"> another page</li>
-            <li><a href ="contact.php"> contact us </li>
-        </ul>
+<div class="body-holder">
+    <div id="header">
+        <h2>Textured-Stars-Light</h2>
+        <h5> Simple. Contemporary. Website Template.</h5>
     </div>
-</header>
+    <header>
+        <div id = "menubar">
+            <ul id = "menu">
+                <li><a href ="#">home</a> </li>
+                <li><a href ="#"> examples</a>  </li>
+                <li><a href ="#"> a page</a> </li>
+                <li><a href ="#"> another page</a> </li>
+                <li><a href ="#"> contact us</a>  </li>
+            </ul>
+        </div>
+    </header>
+    <div class="sections-holder">
+      <div class= "left-content">
+          <h3>Welcome to the textured_stars_light template</h3>
+          <br>
+          <p>
+              This standards compliant, simple, fixed width website template is released as an 'open source' design (under a Creative Commons Attribution 3.0 Licence), which means that you are free to download and use it for anything you want (including modifying and amending it). All I ask is that you leave the 'design from HTML5webtemplates.co.uk' link in the footer of the template, but other than that...
+          </p>
+              <p>
+              This template is written entirely in HTML5 and CSS, and can be validated using the links in the footer.
+              </p>
+          <p>
+              You can view more free HTML5 web templates here.
+          </p>
+          <p>
+              This template is a fully functional 5 page website, with an examples page that gives examples of all the styles available with this design.</p>
+          <h3>Browser Compatibility
+          </h3></h3>
+          <p>This template has been tested in the following browsers:</p>
 
-<div id= "content">
-    <h3>Welcome to the textured_stars_light template</h3>
-    <br>
-    <p>
-        This standards compliant, simple, fixed width website template is released as an 'open source' design (under a Creative Commons Attribution 3.0 Licence), which means that you are free to download and use it for anything you want (including modifying and amending it). All I ask is that you leave the 'design from HTML5webtemplates.co.uk' link in the footer of the template, but other than that...
-    </p>
-        <p>
 
+          <ul>
+              <li>Internet Explorer 9</li>
+              <li>FireFox 25 </li>
+              <li>Google Chrome 31 </li>
+          </ul>
+      </div>
+      <div class="right-content">
+          <h3 id="myId" class="myClass"> Latest News </h3>
+          <h4> New Website Launched </h4> <br/>
 
-        This template is written entirely in HTML5 and CSS, and can be validated using the links in the footer.
-        </p>
-    <p>
-        You can view more free HTML5 web templates here.
-    </p>
-    <p>
-        This template is a fully functional 5 page website, with an examples page that gives examples of all the styles available with this design.</p>
-    <h3>Browser Compatibility
-    </h3></h3>
-    <p>This template has been tested in the following browsers:</p>
+          <p> January 1st, 2014 <br/>
+              2014 sees the redesign of our website. Take a look around and let us know what you think.
+              Read more
+          <h3> Latest News </h3>
+          <h4> New Website Launched </h4> <br/>
 
-    < div id="list" >
-    <ul>
-        <li>Internet Explorer 9</li>
-        <li>FireFox 25 </li>
-        <li>Google Chrome 31 </li>
-    </ul>
-</div>
+          <p> January 1st, 2014 <br/>
+              2014 sees the redesign of our website. Take a look around and let us know what you think.
+              Read more </p>
 
-
-<div id="sidebar">
-    <h3> Latest News </h3>
-    <h4> New Website Launched </h4> <br/>
-
-    <p> January 1st, 2014 <br/>
-        2014 sees the redesign of our website. Take a look around and let us know what you think.
-        Read more
-    <h3> Latest News </h3>
-    <h4> New Website Launched </h4> <br/>
-
-    <p> January 1st, 2014 <br/>
-        2014 sees the redesign of our website. Take a look around and let us know what you think.
-        Read more </p>
-
+      </div>
+    </div>
+    <div class="remove-float"></div>
+    <div class="footer">
+        <p>This is the body footer</p>
+    </div>
 </div>
 </body>
 </html>
